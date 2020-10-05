5b34a9cc591ca81c9bf9e07f22d8db44a3944e50
Arnošt Šenkýř <arnost@senkyr.info>
2020-10-05 20:07:01 +0200

    Extended Light model

--- lights.api/Models/Light.cs
+++ lights.api/Models/Light.cs
@@ -25,13 +25,22 @@ namespace lights.api.Models
         [JsonProperty("bri")]
         public int? Brightness { get; set; }
 
+        [JsonProperty("bri_inc")]
+        public int? BrightnessIncrement { get; set; }
+
         [JsonProperty("ct")]
         public int? ColorTemperature { get; set; }
+
+        [JsonProperty("ct-inc")]
+        public int? ColorTemperatureIncrement { get; set; }
 
         [JsonProperty("alert")]
         public string Alert { get; set; }
 
         [JsonProperty("reachable")]
         public bool IsReachable { get; set; }
+
+        [JsonProperty("scene")]
+        public string SceneId { get; set; }
     }
 }
